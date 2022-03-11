3753e5e1da2eef789f5c88b118fb4d95776a71bd
Matheus Tessaro <tessaro_matheus@hotmail.com>
2022-03-10 21:45:12 -0300

    update ajax

--- src/main/java/br/com/geradorprova/controller/QuestaoController.java
+++ src/main/java/br/com/geradorprova/controller/QuestaoController.java
@@ -10,12 +10,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import br.com.geradorprova.model.Questao;
 import br.com.geradorprova.model.Resposta;
+import br.com.geradorprova.model.enumeration.TipoQuestao;
 import br.com.geradorprova.service.QuestaoService;
 
 @Controller
@@ -72,4 +74,24 @@ public class QuestaoController{
 		return "questao/listar";
 	}
 	
+//	@PostMapping("/teste1")
+//	public String teste1(Questao questao, String tipoQuestao, Model model) {
+//		questao.setTipoQuestao(TipoQuestao.ESCOLHA_MULTIPLA);
+//		model.addAttribute(questao);
+//		return "redirect:/questao/novo :: fragment";
+//	}
+	
+	
+	
+	@PostMapping("/ajax/tipo")
+	public String ajaxLoadTipoQuestao(@ModelAttribute("questao") Questao questao, Model model) {
+		
+		questao.setTipoQuestao(TipoQuestao.ESCOLHA_MULTIPLA);
+		questao.setRespostas(Stream.generate(Resposta::new).limit(5).collect(Collectors.toList()));
+		List<Resposta> respostas = questao.getRespostas();
+		model.addAttribute(questao);
+		model.addAttribute("respostas", respostas);
+	    return "questao/fragment :: teste";
+	}
+
 }
